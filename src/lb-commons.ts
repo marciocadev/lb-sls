2f1574085e5b5a07de9b0749af1ec931d32ff80a
Marcio Almeida <marciocadev@gmail.com>
2022-08-02 14:17:05 -0300

    fix: schema

--- src/lb-commons.ts
+++ src/lb-commons.ts
@@ -11,9 +11,17 @@ export function transformObjToSchema(obj: {[key:string]: any}, schema: {[key:str
   obj.type = 'object';
   obj.properties = {};
   Object.keys(obj).forEach(function(key) {
-    if (Number.isInteger(obj[key])) {schema.properties[key] = { type: 'integer' };} else if (typeof obj[key] === 'number') {schema.properties[key] = { type: 'number' };} else if (typeof obj[key] === 'string') {schema.properties[key] = { type: 'string' };} else if (typeof obj[key] === 'boolean') {schema.properties[key] = { type: 'boolean' };} else if (typeof obj[key] === 'object') {
-      schema.properties[key] = {};
-      transformObjToSchema(obj[key], schema.properties.key);
+    if (Number.isInteger(obj[key])) {
+      schema.properties[key] = { type: 'integer' };
+    } else if (typeof obj[key] === 'number') {
+      schema.properties[key] = { type: 'number' };
+    } else if (typeof obj[key] === 'string') {
+      schema.properties[key] = { type: 'string' };
+    } else if (typeof obj[key] === 'boolean') {
+      schema.properties[key] = { type: 'boolean' };
+    } else if (typeof obj[key] === 'object') {
+      schema.properties = {};
+      transformObjToSchema(obj[key], schema.properties);
     }
   });
 }
